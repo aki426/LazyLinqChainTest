c24f86d63039c35ad87c9a2c3b53fb9c59af1704
aki426 <taoist.aki@gmail.com>
2023-05-21 08:46:20 +0900

    initial commit

---
+++ LazyLinqChain/Logic/LazyLinq.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLinqChain.Logic
+{
+	public class LazyLinq
+	{
+		public static List<(int num1, List<int>)> LinqChain(List<int> list1, List<int> list2)
+		{
+			IEnumerable<LongTimeConstructor> enumerable =
+				list2.Select(num => new LongTimeConstructor(num));
+
+			List<(int num1, List<int>)> result = list1.Select(num1 => (
+				num1,
+				enumerable.Where(num2 => num2.Num == num1).Select(num2 => num2.Num * num1).ToList()
+			)).ToList();
+
+			return result;
+		}
+
+		public static List<(int num1, List<int>)> LinqChainFast(List<int> list1, List<int> list2)
+		{
+			List<LongTimeConstructor> enumerable =
+				list2.Select(num => new LongTimeConstructor(num)).ToList();
+
+			List<(int num1, List<int>)> result = list1.Select(num1 => (
+				num1,
+				enumerable.Where(num2 => num2.Num == num1).Select(num2 => num2.Num * num1).ToList()
+			)).ToList();
+
+			return result;
+		}
+	}
+
+	public class LongTimeConstructor
+	{
+		public int Num { get; set; }
+
+		public LongTimeConstructor(int num)
+		{
+			// await Task.Delay(1000); はコンストラクタ内では使えない？
+			System.Threading.Thread.Sleep(100);
+			Num = num;
+		}
+	}
+}
---
+++ LazyLinqChainTests/Logic/LazyLinqTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LazyLinqChain.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LazyLinqChain.Logic.Tests
+{
+	[TestClass()]
+	public class LazyLinqTests
+	{
+		[TestMethod()]
+		public void LinqChainTest()
+		{
+			List<int> lis1 = Enumerable.Range(0, 10).ToList();
+			List<int> lis2 = Enumerable.Range(0, 10).ToList();
+
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+			LazyLinq.LinqChain(lis1, lis2);
+			stopWatch.Stop();
+
+			// Green
+			Assert.IsTrue(stopWatch.Elapsed.TotalSeconds < 12.0);
+			// Red
+			Assert.IsTrue(stopWatch.Elapsed.TotalSeconds < 2.0);
+		}
+
+		[TestMethod()]
+		public void LinqChainFastTest()
+		{
+			List<int> lis1 = Enumerable.Range(0, 10).ToList();
+			List<int> lis2 = Enumerable.Range(0, 10).ToList();
+
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+			LazyLinq.LinqChainFast(lis1, lis2);
+			stopWatch.Stop();
+
+			// Green
+			Assert.IsTrue(stopWatch.Elapsed.TotalSeconds < 12.0);
+			// Green
+			Assert.IsTrue(stopWatch.Elapsed.TotalSeconds < 2.0);
+		}
+	}
+}
